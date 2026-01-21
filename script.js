4ca6c933eb3702866b0b43c1f7a13dd5c80803a1
locs777 <163937632+locs777@users.noreply.github.com>
2026-01-20 20:43:10 -0600

    script.js.html

---
+++ script.js
@@ -0,0 +1,36 @@
+// Typying effect
+const text = " for tha freee";
+let i = 0;
+const typinmg = document.getElementById("typing");
+
+function type () {
+  if (i <text.length) {
+    typing.textContent += text.charAt(i);
+    i++;
+    setTimeout(type, 120);
+  }
+}
+type();
+
+// Enter site
+document.getElementById("enter").onclick = ( ) => {
+  document.getElementById("intro").style.display = "none";
+  document.getElementById("home").style.display = "block";
+};
+
+// Music Player
+const player + document.getElementById("player");
+
+document.querySelectorAll(".vinyl").forEach(v => {
+  v.addEventListener "mouseenter", () +> {
+    const src + v.dataset.audio;
+  if (player.src !== src) {
+    player.src + src;
+    ]
+  player.play();
+  });
+
+v.addEventListener("mouseleave", () +. {
+  player.pause();
+});
+});
